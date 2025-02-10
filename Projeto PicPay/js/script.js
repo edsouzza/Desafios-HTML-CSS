8ca3a2942f922383b8914f47477050d1b2a5d3b6
Edi Aquino de Souza <edsouzza@hotmail.com>
2025-02-10 17:14:09 -0300

    Delete Projeto PicPay directory

--- Projeto PicPay/js/script.js
+++
@@ -1,9 +0,0 @@
-const input = document.querySelector('#campoCPF');
-
-input.addEventListener('focus', function (){
-    input.setAttribute('class', 'placeholder-grayDark');
-})
-
-input.addEventListener('blur', function (){
-    input.setAttribute('class', 'placeholder-gray');
-})
