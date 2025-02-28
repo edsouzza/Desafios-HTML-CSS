7a00869061e237a4540df5eeb12e18e4db273aab
Edi Aquino de Souza <edsouzza@hotmail.com>
2025-02-28 14:04:11 -0300

    feat : maked section um cartao para cada estilo

--- Projeto Clone PicPay Mobile First/js/scripts.js
+++ Projeto Clone PicPay Mobile First/js/scripts.js
@@ -2,13 +2,16 @@
 const menuIcon   = document.querySelector('.menu-icon')
 const menuList   = document.querySelector('.nav-list')
 const imgMenu    = document.querySelector('.menu-icon img')
+const body       = document.querySelector('body')
 
 menuIcon.addEventListener('click',() =>{
     if(menuList.classList.contains('ativo')){
         menuList.classList.remove('ativo')
         imgMenu.src = './images/menu/menu.png'
+        body.style.overflowY = 'auto'
     }else{
         menuList.classList.add('ativo')
         imgMenu.src = './images/menu/close.png'
+        body.style.overflowY = 'hidden'
     }   
 })
